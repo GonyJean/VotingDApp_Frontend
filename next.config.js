0b00147df000eedf214e849041cf55a66f98c2d3
Yxj <gony952701@gmail.com>
2024-03-17 20:23:25 +0800

    feat: add assetPrefix

--- next.config.js
+++ next.config.js
@@ -2,17 +2,18 @@
 * @Author: Yxj
 * @LastEditors: Yxj
 * @Date: 2024-03-12 19:03:54
-* @LastEditTime: 2024-03-17 20:06:49
+* @LastEditTime: 2024-03-17 20:21:10
 * @Description: file content
 * @FilePath: \next.config.js
  **/
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
+console.log(isProd,'@@@');
 const nextConfig = {
   output: 'export',
   reactStrictMode: true,
-   env: {
-    basePath: '/VotingDApp_Frontend',
-    assetPrefix: '/VotingDApp_Frontend/',
+  assetPrefix: isProd ? 'https://gonyjean.github.io/VotingDApp_Frontend/' : '',
+  env: {
     NEXT_PUBLIC_ENABLE_TESTNETS: 'true',
   }
 };
